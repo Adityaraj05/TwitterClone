a6cbfad0b2f676356773a93ed23b14310d38bb23
Aditya Raj <adityarajavi05@gmmail.com>
2024-05-16 21:13:51 +0530

    updated in sidebar menu

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -44,13 +44,13 @@ export default function Sidebar() {
 
       <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
         <img
-          src="https://www.adscientificindex.com/pictures/0b/50734.jpg"
+          src="https://yt3.ggpht.com/M9SMtoHULi1LF3DvkJ6PeVInfEwWyhjBU6Drf-Z9kmqRc6SWoYfY4rOhe2n9SQ26qPTn3u_odqI=s88-c-k-c0x00ffffff-no-rj-mo"
           alt="user-img"
           className="h-10 w-10 rounded-full xl:mr-2"
         />
         <div className="leading-5 hidden xl:inline">
-          <h4 className="font-bold">Sahand Ghavidel</h4>
-          <p className="text-gray-500">@codewithsahand</p>
+          <h4 className="font-bold">Aditya Raj</h4>
+          <p className="text-gray-500">@Adityarajavi05</p>
         </div>
         <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline"/>
       </div>
