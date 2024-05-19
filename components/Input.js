691ee17d7f6eeae789bf8fd0f10feb9e88ce507b
Aditya Raj <adityarajavi05@gmmail.com>
2024-05-19 18:22:12 +0530

    final soltion of error

--- components/Input.js
+++ components/Input.js
@@ -11,27 +11,31 @@ import {
   updateDoc,
 } from "firebase/firestore";
 import { getDownloadURL, ref, uploadString } from "firebase/storage";
-import { useSession, signOut } from "next-auth/react";
+
 import { useState, useRef } from "react";
 import { db, storage } from "../firebase";
+import { useRecoilState } from "recoil";
+import { userState } from "../atom/userAtom";
+import { signOut, getAuth } from "firebase/auth";
 export default function Input() {
-  const { data: session } = useSession();
   const [input, setInput] = useState("");
+  const [currentUser, setCurrentUser] = useRecoilState(userState);
   const [selectedFile, setSelectedFile] = useState(null);
   const [loading, setLoading] = useState(false);
   const filePickerRef = useRef(null);
+  const auth = getAuth();
 
   const sendPost = async () => {
     if (loading) return;
     setLoading(true);
 
     const docRef = await addDoc(collection(db, "posts"), {
-      id: session.user.uid,
+      id: currentUser.uid,
       text: input,
-      userImg: session.user.image,
+      userImg: currentUser.userImg,
       timestamp: serverTimestamp(),
-      name: session.user.name,
-      username: session.user.username,
+      name: currentUser.name,
+      username: currentUser.username,
     });
 
     const imageRef = ref(storage, `posts/${docRef.id}/image`);
@@ -61,17 +65,22 @@ export default function Input() {
     };
   };
 
+  function onSignOut() {
+    signOut(auth);
+    setCurrentUser(null);
+  }
+
   return (
     <>
-      {session && (
-        <div className="flex  border-b border-gray-300 p-3 space-x-3">
+      {currentUser && (
+        <div className="flex  border-b border-gray-200 p-3 space-x-3">
           <img
-            onClick={signOut}
-            src={session.user.image}
+            onClick={onSignOut}
+            src={currentUser?.userImg}
             alt="user-img"
             className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
           />
-          <div className="w-full divide-y divide-gray-300">
+          <div className="w-full divide-y divide-gray-200">
             <div className="">
               <textarea
                 className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700"
@@ -85,7 +94,7 @@ export default function Input() {
               <div className="relative">
                 <XIcon
                   onClick={() => setSelectedFile(null)}
-                  className=" border h-7 text-black absolute cursor-pointer shadow-md border-white m-1 rounded-full"
+                  className="border h-7 text-black absolute cursor-pointer shadow-md border-white m-1 rounded-full"
                 />
                 <img
                   src={selectedFile}
