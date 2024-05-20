0ac2e6e76623c8aecdf3f0679eedb5f4da818059
Aditya Raj <adityarajavi05@gmmail.com>
2024-05-20 08:31:38 +0530

    fixing error

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -55,7 +55,6 @@ export default function PostPage({ newsResults, randomUsersResults }) {
         <Sidebar />
 
         {/* Feed */}
-
         <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
           <div className="flex items-center space-x-2  py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
             <div className="hoverEffect" onClick={() => router.push("/")}>
